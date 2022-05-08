e9467d7d604a8e50f5e7db402f204b8a0265ab1b
LeonAngelit <leo63582@gmail.com>
2022-05-08 14:11:48 +0200

    cuarto commit

--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const useGetProducts = (API) => {
   const [products, setProducts] = useState([]);
-  useEffect(() => async () => {
+  useEffect(async () => {
       const response = await axios(API);
       setProducts(response.data);
     },
