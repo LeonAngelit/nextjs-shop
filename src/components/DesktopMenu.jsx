1b722bf4467d909f40e296e8963145fcb8dfad63
LeonAngelit <leo63582@gmail.com>
2022-05-19 13:05:13 +0200

    accesibilidad

--- src/components/DesktopMenu.jsx
+++ src/components/DesktopMenu.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import styles from '@styles/DesktopMenu.module.scss';
-import Link from 'next/link';
-const DesktopMenu = ({handle}) => {
-    return ( <nav className={styles.menu}>
-  <ul>
-    <li onClick={handle}><Link href={"/my-orders"}>My orders</Link></li>
-    <li onClick={handle}><Link href={"/my-account"}>My account</Link></li>
-    <li onClick={handle}><Link href={"/"} className={styles["sign-out"]}>Sign out</Link></li>
-  </ul>
-</nav>
- );
+import React from "react";
+import styles from "@styles/DesktopMenu.module.scss";
+import Link from "next/link";
+const DesktopMenu = ({ handle }) => {
+  return (
+    <nav className={styles.menu}>
+      <ul>
+        <li>
+          <div onClick={handle} onKeyDown={handle} role="button" tabIndex={0}>
+            <Link href={"/my-orders"}>My orders</Link>
+          </div>
+        </li>
+        <li>
+          <div onClick={handle} onKeyDown={handle} role="button" tabIndex={0}>
+            <Link href={"/my-account"}>My account</Link>
+          </div>
+        </li>
+        <li>
+          <div onClick={handle} onKeyDown={handle} role="button" tabIndex={0}>
+            <Link href={"/"} className={styles["sign-out"]}>
+              Sign out
+            </Link>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  );
 };
- 
+
 export default DesktopMenu;
--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,30 +1,49 @@
-import React from 'react';
-import styles from '@styles/MobileMenu.module.scss';
-import Link from 'next/link';
+import React from "react";
+import styles from "@styles/MobileMenu.module.scss";
+import Link from "next/link";
 
-const MobileMenu = ({handle}) => {
-    return ( 
-<nav className={styles['mobile-menu']}>
-  <h2>CATEGORIES</h2>
-  <ul className={styles.section1}>
-    <li><Link href="">All</Link></li>
-    <li><Link href="">Clothes</Link></li>
-    <li><Link href="">Electronics</Link></li>
-    <li><Link href="">Furniture</Link></li>
-    <li><Link href="">Toys</Link></li>
-    <li><Link href="">Others</Link></li>
-  </ul>
-  <ul className={styles.section2}>
-    <li onClick={handle}><Link href="/my-orders">My orders</Link></li>
-    <li onClick={handle}><Link href="/my-account">My account</Link></li>
-  </ul>
-  <div className={styles['session-info']}>
-    <p>camilayakoo@gmail.com</p>
-    <Link href="">Sign out</Link>
-  </div>
-</nav>
-
-     );
+const MobileMenu = ({ handle }) => {
+  return (
+    <nav className={styles["mobile-menu"]}>
+      <h2>CATEGORIES</h2>
+      <ul className={styles.section1}>
+        <li>
+          <Link href="">All</Link>
+        </li>
+        <li>
+          <Link href="">Clothes</Link>
+        </li>
+        <li>
+          <Link href="">Electronics</Link>
+        </li>
+        <li>
+          <Link href="">Furniture</Link>
+        </li>
+        <li>
+          <Link href="">Toys</Link>
+        </li>
+        <li>
+          <Link href="">Others</Link>
+        </li>
+      </ul>
+      <ul className={styles.section2}>
+        <li>
+          <div onClick={handle} onKeyDown={handle} role="button" tabIndex={0}>
+            <Link href="/my-orders">My orders</Link>
+          </div>
+        </li>
+        <li>
+          <div onClick={handle} onKeyDown={handle} role="button" tabIndex={0}>
+            <Link href="/my-account">My account</Link>
+          </div>
+        </li>
+      </ul>
+      <div className={styles["session-info"]}>
+        <p>camilayakoo@gmail.com</p>
+        <Link href="">Sign out</Link>
+      </div>
+    </nav>
+  );
 };
- 
+
 export default MobileMenu;
